cb857b37c275d956273a2ab80a498501a8d4f88f
Vitalik <vitalik.yatsenko@jetup.team>
2023-08-26 16:43:52 +0300

    test

--- src/modules/childrens/api/childrens-calendars.api.ts
+++ src/modules/childrens/api/childrens-calendars.api.ts
@@ -10,7 +10,7 @@ class ChildrensCalendarsAPI extends API {
 	protected basePath = 'childrensCalendars'
 
 	public getOne(params: GetOneChildrensCalendarParams) {
-		return this.call('getOne', params)
+		return this.calls('getOne', params)
 	}
 
 	public put(payload: IChildrenCalendarRecord) {
